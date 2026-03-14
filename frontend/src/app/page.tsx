61ad151cb991f666c4530ce5cae699834dcc8d7d
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2026-03-15 03:10:42 +0700

    feat: implement hero section in landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,64 +1,36 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex min-h-screen flex-col bg-[#EDEBDF]">
+      <main>
+        <section className="container mx-auto px-4 py-32">
+          <div className="mx-auto max-w-5xl">
+            <h1 className="text-center text-6xl font-bold text-[#3D5690] mb-5">
+              Your Drink, Ready When You Are
+            </h1>
+            <p className="text-center text-lg font-medium text-[#3D5690] mb-10">
+              Skip the wait, not the quality. Order ahead and pick up your drink exactly when it's ready.
+            </p>
+            <div className="flex flex-row p-6 bg-[#D9D9D9] rounded-[20px]">
+              <div className="flex flex-col w-3/4 mx-auto px-10 py-20">
+                <h2 className="text-3xl font-bold text-[#3D5690] mb-3">
+                  Why SkipLine?
+                </h2>
+                <p className="text-[#3D5690] mb-3">
+                  SkipLine is a smarter way to get your daily drinks. We use a real-time queue system <br /> so you always know exactly how long your order will take
+                  without guessing, waiting in line. Just order, show up, and enjoy.
+                </p>
+                <button className="text-start bg-[#3D5690] text-[#EDEBDF] rounded-[5px] px-4 py-2 w-fit">
+                  Order Now
+                </button>
+              </div>
+              <div className="w-1/4 mx-auto">
+                <Image src="/kid-herosection.png" alt="Kid holding a cup" width={200} height={200} />
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
